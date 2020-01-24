543c435132e90b74351573ccdb98060b870dc806
max <maximohinojosa24@yahoo.com>
2020-01-23 16:06:25 -0800

    [init]

---
+++ TopNews/TopNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopNews
+//
+//  Created by Student Loaner 3 on 1/23/20.
+//  Copyright © 2020 Maximo Hinojosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
